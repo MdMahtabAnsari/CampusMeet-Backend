79d3a62681e04994397dec73328b159083cc821b
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2024-07-29 12:06:17 +0530

    implemeted user repository

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,43 @@
+const User = require('../models/userModel');
+const BadRequestError = require('../utils/errors/badRequestError');
+const InternalServerError = require('../utils/errors/internalServerError');
+
+class UserRepository {
+    async createUser(user) {
+        try {
+            return await User.create(user);
+        }
+        catch (error) {
+            console.log(error);
+            if (error.name === 'ValidationError') {
+                const errors = Object.values(error.errors).map((error) => error.message);
+                throw new BadRequestError(errors);
+            }
+            else {
+                throw new InternalServerError();
+            }
+        }
+    }
+
+    async getUserByEmail(email) {
+        try {
+            return await User.findOne({ email: email });
+        }
+        catch (error) {
+            console.log(error);
+            throw new InternalServerError();
+        }
+    }
+
+    async getUserById(id) {
+        try {
+            return await User.findById(id);
+        }
+        catch (error) {
+            console.log(error);
+            throw new InternalServerError();
+        }
+    }
+}
+
+module.exports = UserRepository;
