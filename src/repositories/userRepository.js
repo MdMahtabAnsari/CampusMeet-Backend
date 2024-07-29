52da83434868c6837255edcd1b646e39242d082b
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2024-07-29 14:47:00 +0530

    handle conflict error

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const BadRequestError = require('../utils/errors/badRequestError');
 const InternalServerError = require('../utils/errors/internalServerError');
+const ConflictError = require('../utils/errors/conflictError');
 
 class UserRepository {
     async createUser(user) {
@@ -12,6 +13,9 @@ class UserRepository {
             if (error.name === 'ValidationError') {
                 const errors = Object.values(error.errors).map((error) => error.message);
                 throw new BadRequestError(errors);
+            }
+            else if (error.name === 'MongoServerError') {
+                throw new ConflictError('Email or Phone number');
             }
             else {
                 throw new InternalServerError();
