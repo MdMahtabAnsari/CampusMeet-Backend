b615b3553a04bd7c293d823b98934988d65d166d
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2024-07-29 12:33:12 +0530

    initial setup of server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const dbConfig = require('./configs/dbConfig');
+const serverConfig = require('./configs/serverConfig');
+const userRouter = require('./routers/userRouter');
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use('/users', userRouter);
+
+
+app.listen(serverConfig.PORT, async () => {
+    await dbConfig.connectToDB();
+    console.log(`Server running on port ${serverConfig.PORT}`);
+});
+
---
+++ src/routers/userRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const userRouter = express.Router();
+
+userRouter.post('/signup', userController.signup);
+
+module.exports = userRouter;
