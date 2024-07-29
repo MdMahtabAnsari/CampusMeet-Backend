0baf71dab54066eb29b190c3edc8bcb7c7d591c0
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2024-07-29 14:47:40 +0530

    fix bug

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,6 +2,6 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const userRouter = express.Router();
 
-userRouter.post('/signup', userController.signup);
+userRouter.post('/signup', userController.createUser);
 
 module.exports = userRouter;
