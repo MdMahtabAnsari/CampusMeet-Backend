525402eabce16c141cb4d0095b3d8de2d76548b9
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2024-09-01 18:30:43 +0530

    chore: Update Redis configuration

--- src/configs/queueConfig.js
+++ src/configs/queueConfig.js
@@ -1,29 +1,29 @@
 const Queue = require("bull");
-const serverConfig = require("./serverConfig");
+const redis = require('./redisConfig')
 
-const redisConfig = {
-  host: serverConfig.REDIS_HOST,
-  port: serverConfig.REDIS_PORT,
-  password: serverConfig.REDIS_PASSWORD,
-};
+// const redisConfig = {
+//   host: serverConfig.REDIS_HOST,
+//   port: serverConfig.REDIS_PORT,
+//   password: serverConfig.REDIS_PASSWORD,
+// };
 const statusQueue = new Queue("status", {
-  redis: redisConfig,
+  redis: redis,
 });
 
 const otpQueue = new Queue("otp", {
-  redis: redisConfig,
+  redis: redis,
 });
 
 const userQueue = new Queue("user", {
-  redis: redisConfig,
+  redis: redis,
 });
 
 const imageQueue = new Queue("image", {
-  redis: redisConfig,
+  redis: redis,
 });
 
 const friendQueue = new Queue("friend", {
-  redis: redisConfig,
+  redis: redis,
 });
 
 module.exports = {
