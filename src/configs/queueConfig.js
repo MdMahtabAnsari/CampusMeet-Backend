049a479be198c1d66bc567edbeb87fd7303a7d58
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2024-09-02 13:41:38 +0530

    chore: Remove redundant event listeners in queueConfig.js

--- src/configs/queueConfig.js
+++ src/configs/queueConfig.js
@@ -37,46 +37,6 @@ friendQueue.on("error", (error) => {
   console.log("Error with friend queue", error);
 });
 
-statusQueue.on("completed", (job) => {
-  console.log("Status job completed",job.data);
-});
-
-otpQueue.on("completed", (job) => {
-  console.log("OTP job completed", job.data);
-});
-
-userQueue.on("completed", (job) => {
-  console.log("User job completed", job.data);
-});
-
-imageQueue.on("completed", (job) => {
-  console.log("Image job completed", job.data );
-});
-
-friendQueue.on("completed", (job) => {
-  console.log("Friend job completed", job.data);
-});
-
-statusQueue.on("failed", (job) => {
-  console.log("Status job failed", job.data);
-});
-
-otpQueue.on("failed", (job) => {
-  console.log("OTP job failed",job.data);
-});
-
-userQueue.on("failed", (job) => {
-  console.log("User job failed", job.data);
-});
-
-imageQueue.on("failed", (job) => {
-  console.log("Image job failed",job.data );
-});
-
-friendQueue.on("failed", (job) => {
-  console.log("Friend job failed", job.data);
-});
-
 
 module.exports = {
   statusQueue,
